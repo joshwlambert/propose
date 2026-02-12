f0104729476f4bc06ad858f1da708e7d84ce3b2d
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2026-02-12 15:46:47 +0000

    arrange scenario_sim() parameters into grid

--- app.R
+++ app.R
@@ -4,30 +4,44 @@ library(ringbp)
 ui <- fluidPage(
   titlePanel("{propose}: a Shiny app for {ringbp}"),
 
-  "Offspring distribution parameters: ",
-  numericInput("community_r0", "Community R0:", value = 2),
-  numericInput("community_disp", "Community Dispersion:", value = 1),
-  numericInput("isolated_r0", "Isolated R0:", value = 0),
-  numericInput("isolated_disp", "Isolated Dispersion:", value = 1),
-
-  "Delay distribution parameters: ",
-  numericInput("incubation_meanlog", "Incubation period meanlog:", value = 1.5),
-  numericInput("incubation_sdlog", "Incubation period sdlog:", value = 0.4),
-  numericInput("onset_to_isolation_meanlog", "Onset-to-isolation meanlog:", value = 2),
-  numericInput("onset_to_isolation_sdlog", "Onset-to-isolation sdlog:", value = 0.5),
-
-  "Event probabilities: ",
-  numericInput("asymptomatic", "Probability asymptomatic:", value = 0.1),
-  numericInput("presymptomatic_transmission", "Probability of presymptomatic transmission:", value = 0.1),
-  numericInput("symptomatic_ascertained", "Probability of contact traced:", value = 0.8),
-
-  "Interventions: ",
-  textInput("quarantine", "Quarantine:", value = "FALSE"),
-
-  "Simulation controls: ",
-  numericInput("cap_max_days", "Maximum number of days:", value = 100),
-  numericInput("cap_cases", "Maximum number of cases:", value = 5000),
-
+  fluidRow(
+    column(
+      4,
+      "Offspring distribution parameters: ",
+      numericInput("community_r0", "Community R0:", value = 2),
+      numericInput("community_disp", "Community Dispersion:", value = 1),
+      numericInput("isolated_r0", "Isolated R0:", value = 0),
+      numericInput("isolated_disp", "Isolated Dispersion:", value = 1)
+    ),
+    column(
+      4,
+      "Delay distribution parameters: ",
+      numericInput("incubation_meanlog", "Incubation period meanlog:", value = 1.5),
+      numericInput("incubation_sdlog", "Incubation period sdlog:", value = 0.4),
+      numericInput("onset_to_isolation_meanlog", "Onset-to-isolation meanlog:", value = 2),
+      numericInput("onset_to_isolation_sdlog", "Onset-to-isolation sdlog:", value = 0.5)
+    ),
+    column(
+      4,
+      "Event probabilities: ",
+      numericInput("asymptomatic", "Probability asymptomatic:", value = 0.1),
+      numericInput("presymptomatic_transmission", "Probability of presymptomatic transmission:", value = 0.1),
+      numericInput("symptomatic_ascertained", "Probability of contact traced:", value = 0.8)
+    )
+  ),
+  fluidRow(
+    column(
+      6,
+      "Interventions: ",
+      textInput("quarantine", "Quarantine:", value = "FALSE")
+    ),
+    column(
+      6,
+      "Simulation controls: ",
+      numericInput("cap_max_days", "Maximum number of days:", value = 100),
+      numericInput("cap_cases", "Maximum number of cases:", value = 5000)
+    )
+  ),
   "Probability of outbreak extinction: ",
   verbatimTextOutput("extinct")
 )
