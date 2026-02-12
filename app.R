a10bca5b8678b52951b1674f5df84c7f5372b4a8
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2026-02-12 15:56:24 +0000

    add slider for simulation replicates and sidebar layout

--- app.R
+++ app.R
@@ -5,53 +5,60 @@ library(tinyplot)
 ui <- fluidPage(
   titlePanel("{propose}: a Shiny app for {ringbp}"),
 
-  fluidRow(
-    column(
-      4,
-      "Offspring distribution parameters: ",
-      numericInput("community_r0", "Community R0:", value = 2),
-      numericInput("community_disp", "Community Dispersion:", value = 1),
-      numericInput("isolated_r0", "Isolated R0:", value = 0),
-      numericInput("isolated_disp", "Isolated Dispersion:", value = 1)
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("replicates", "Number of simulation replicates", min = 1, max = 100, value = 10)
     ),
-    column(
-      4,
-      "Delay distribution parameters: ",
-      numericInput("incubation_meanlog", "Incubation period meanlog:", value = 1.5),
-      numericInput("incubation_sdlog", "Incubation period sdlog:", value = 0.4),
-      numericInput("onset_to_isolation_meanlog", "Onset-to-isolation meanlog:", value = 2),
-      numericInput("onset_to_isolation_sdlog", "Onset-to-isolation sdlog:", value = 0.5)
-    ),
-    column(
-      4,
-      "Event probabilities: ",
-      numericInput("asymptomatic", "Probability asymptomatic:", value = 0.1),
-      numericInput("presymptomatic_transmission", "Probability of presymptomatic transmission:", value = 0.1),
-      numericInput("symptomatic_ascertained", "Probability of contact traced:", value = 0.8)
+    mainPanel(
+      fluidRow(
+        column(
+          4,
+          "Offspring distribution parameters: ",
+          numericInput("community_r0", "Community R0:", value = 2),
+          numericInput("community_disp", "Community Dispersion:", value = 1),
+          numericInput("isolated_r0", "Isolated R0:", value = 0),
+          numericInput("isolated_disp", "Isolated Dispersion:", value = 1)
+        ),
+        column(
+          4,
+          "Delay distribution parameters: ",
+          numericInput("incubation_meanlog", "Incubation period meanlog:", value = 1.5),
+          numericInput("incubation_sdlog", "Incubation period sdlog:", value = 0.4),
+          numericInput("onset_to_isolation_meanlog", "Onset-to-isolation meanlog:", value = 2),
+          numericInput("onset_to_isolation_sdlog", "Onset-to-isolation sdlog:", value = 0.5)
+        ),
+        column(
+          4,
+          "Event probabilities: ",
+          numericInput("asymptomatic", "Probability asymptomatic:", value = 0.1),
+          numericInput("presymptomatic_transmission", "Probability of presymptomatic transmission:", value = 0.1),
+          numericInput("symptomatic_ascertained", "Probability of contact traced:", value = 0.8)
+        )
+      ),
+      fluidRow(
+        column(
+          6,
+          "Interventions: ",
+          textInput("quarantine", "Quarantine:", value = "FALSE")
+        ),
+        column(
+          6,
+          "Simulation controls: ",
+          numericInput("cap_max_days", "Maximum number of days:", value = 100),
+          numericInput("cap_cases", "Maximum number of cases:", value = 5000)
+        )
+      ),
+      "Probability of outbreak extinction: ",
+      verbatimTextOutput("extinct"),
+      plotOutput("cumulative_cases")
     )
-  ),
-  fluidRow(
-    column(
-      6,
-      "Interventions: ",
-      textInput("quarantine", "Quarantine:", value = "FALSE")
-    ),
-    column(
-      6,
-      "Simulation controls: ",
-      numericInput("cap_max_days", "Maximum number of days:", value = 100),
-      numericInput("cap_cases", "Maximum number of cases:", value = 5000)
-    )
-  ),
-  "Probability of outbreak extinction: ",
-  verbatimTextOutput("extinct"),
-  plotOutput("cumulative_cases")
+  )
 )
 
 server <- function(input, output, session) {
   scenario <- reactive({
     scenario_sim(
-      n = 1,
+      n = input$replicates,
       initial_cases = 5,
       offspring = offspring_opts(
         community = \(n) rnbinom(n = n, mu = input$community_r0, size = input$community_disp),
