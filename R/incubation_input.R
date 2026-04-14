a00e6253044f1a9bc7b42f71e2ae68e8b71e5294
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2026-04-14 11:38:31 +0100

    add incubation_period_input function, relates #37

---
+++ R/incubation_input.R
@@ -0,0 +1,53 @@
+#' Generate [bslib::accordion()] with inputs for parameterising
+#' `incubation_period` argument in [ringbp::delay_opts()]
+#'
+#' @param ns A namespace created with [shiny::NS()].
+#' @param ... [dots] Not used, will throw a warning if arguments are supplied.
+#'
+#' @return A [bslib::accordion()] object.
+#' @keywords internal
+incubation_input <- function(ns, ...) {
+  chkDots(...)
+  accordion(
+    accordion_panel(
+      title = "Incubation period distribution parameters:",
+      icon = bs_icon("hourglass-split"),
+      selectInput(
+        inputId = ns("incubation_distribution"),
+        label = "Incubation Period Distribution",
+        choices = list(
+          "Lognormal" = "lnorm",
+          "Gamma" = "gamma",
+          "Weibull" = "weibull"
+        )
+      ),
+      conditionalPanel(
+        condition = "input.incubation_distribution == 'lnorm'",
+        numericInput(
+          ns("incubation_meanlog"),
+          "Incubation period meanlog:",
+          value = PROPOSE_DEFAULTS$incubation_meanlog
+        ),
+        numericInput(
+          ns("incubation_sdlog"),
+          "Incubation period sdlog:",
+          value = PROPOSE_DEFAULTS$incubation_sdlog
+        ),
+        ns = ns
+      ),
+      conditionalPanel(
+        condition = "input.incubation_distribution == 'gamma'",
+        numericInput(ns("incubation_shape"), "Incubation period shape:", value = 2),
+        numericInput(ns("incubation_scale"), "Incubation period scale:", value = 1),
+        ns = ns
+      ),
+      conditionalPanel(
+        condition = "input.incubation_distribution == 'weibull'",
+        numericInput(ns("incubation_shape"), "Incubation period shape:", value = 2),
+        numericInput(ns("incubation_scale"), "Incubation period scale:", value = 1),
+        ns = ns
+      )
+    ),
+    open = FALSE
+  )
+}
