020233980cca872b8506cbcddc0b3f1176407264
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2026-04-14 11:39:45 +0100

    add symptom_event_prob_input function, relates #37

---
+++ R/symptom_event_prob_input.R
@@ -0,0 +1,27 @@
+#' Generate [bslib::accordion()] with inputs for parameterising `asymptomatic`
+#' and `presymptomatic_transmission` arguments in [ringbp::event_prob_opts()]
+#'
+#' @param ns A namespace created with [shiny::NS()].
+#' @param ... [dots] Not used, will throw a warning if arguments are supplied.
+#'
+#' @return A [bslib::accordion()] object.
+#' @keywords internal
+symptom_event_prob_input <- function(ns, ...) {
+  accordion(
+    accordion_panel(
+      title = "Symptom event probabilities:",
+      icon = bs_icon("person-fill-gear"),
+      numericInput(
+        ns("asymptomatic"),
+        "Probability asymptomatic:",
+        value = PROPOSE_DEFAULTS$asymptomatic
+      ),
+      numericInput(
+        ns("presymptomatic_transmission"),
+        "Probability of presymptomatic transmission:",
+        value = PROPOSE_DEFAULTS$presymptomatic_transmission
+      )
+    ),
+    open = FALSE
+  )
+}
