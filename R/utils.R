5cccb0cd2bc496e2badb32ad04fa8cb6c942c870
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2026-04-14 09:05:23 +0100

    add event prob defaults to PROPOSE_DEFAULTS, relates #32

--- R/utils.R
+++ R/utils.R
@@ -7,6 +7,10 @@
 
 #' @rdname defaults
 PROPOSE_DEFAULTS <- list(
+  # event probs
+  asymptomatic = 0.1,
+  presymptomatic_transmission = 0.1,
+  symptomatic_traced = 0.8,
   # intervention
   quarantine = FALSE
 )
