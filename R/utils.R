2c4af5ad5efdc9ffd22406c8718cc665527a18b3
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2026-04-14 08:52:22 +0100

    add PROPOSE_DEFAULTS list, relates #32

---
+++ R/utils.R
@@ -0,0 +1,12 @@
+#' \pkg{ringbp} simulation defaults used in \pkg{propose}
+#'
+#' @name defaults
+#'
+#' @description
+#' `PROPOSE_DEFAULTS` is a list with the default outbreak simulation parameters.
+
+#' @rdname defaults
+PROPOSE_DEFAULTS <- list(
+  # intervention
+  quarantine = FALSE
+)
